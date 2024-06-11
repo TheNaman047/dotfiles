94be5f166232039e9a28ff1f43d4988e4c864a42
TheNaman047 <namankhandelwal314@gmail.com>
2024-06-11 11:07:49 +0530

    feat: keymaps updated

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -20,11 +20,11 @@ return {
     })
     local opts = { noremap = true, silent = true }
     -- Bufferline cycle through buffers
-    vim.api.nvim_set_keymap("n", "gbp", ":lua require'bufferline'.cycle(1)<CR>", opts)
-    vim.api.nvim_set_keymap("n", "gbn", ":lua require'bufferline'.cycle(-1)<CR>", opts)
+    vim.api.nvim_set_keymap("n", "gjn", ":lua require'bufferline'.cycle(1)<CR>", opts)
+    vim.api.nvim_set_keymap("n", "gjp", ":lua require'bufferline'.cycle(-1)<CR>", opts)
     -- Bufferline close buffer
-    vim.api.nvim_set_keymap("n", "gbd", ":bd<CR>", opts)
+    vim.api.nvim_set_keymap("n", "gjd", ":bd<CR>", opts)
     -- Bufferline save buffer
-    vim.api.nvim_set_keymap("n", "gbs", ":w<CR>", opts)
+    vim.api.nvim_set_keymap("n", "gjs", ":w<CR>", opts)
   end,
 }
