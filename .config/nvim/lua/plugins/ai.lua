c91113750793f264fe0a6eed1fb325d6f9ae3309
TheNaman047 <namankhandelwal314@gmail.com>
2024-08-20 00:38:42 +0530

    feat: refactored ai.lua, added cmp-ai base config

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,84 +1,103 @@
 -- load ollama status functions
 local ollama_prompts = require("config.ollama-prompts")
 
+local ollama_url = "https://api-ollama-local.thenaman047.dev"
+local ollama_model = "llama3"
+local gen_nvim_model = "codestral-latest"
+local gen_nvim_host = "codestral.mistral.ai"
+local mistral_curl = function(mistralAPIKey)
+	return "curl --silent --no-buffer --header 'Authorization: Bearer "
+		.. mistralAPIKey
+		.. "' -X POST https://"
+		.. gen_nvim_host
+		.. "/v1/chat/completions -d $body"
+end
+local ollama_curl = "curl --silent --no-buffer -X POST https://" .. gen_nvim_host .. "/api/chat -d $body"
+
+local ollama_config = {
+	"nomnivore/ollama.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+	keys = {
+		{
+			"<leader>oo",
+			":<c-u>lua require('ollama').prompt()<cr>",
+			desc = "ollama prompt",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>oa",
+			":<c-u>lua require('ollama').prompt('Ask_About_Code')<cr>",
+			desc = "ollama Generate Code",
+			mode = { "n", "v" },
+		},
+	},
+	opts = {
+		model = ollama_model,
+		url = ollama_url,
+		prompts = ollama_prompts.prompts,
+	},
+}
+
+local gen_nvim_config = {
+	"David-Kunz/gen.nvim",
+	opts = {
+		model = gen_nvim_model,
+		host = gen_nvim_host,
+		port = "",
+		command = function(options)
+			local body = { model = options.model, stream = true }
+			local mistralAPIKey = os.getenv("MISTRAL_API_KEY")
+			if not mistralAPIKey then
+				print("Error: MISTRAL_API_KEY environment variable is not set.")
+				return nil
+			end
+			local endpoint = mistral_curl(mistralAPIKey)
+			return endpoint
+		end,
+		display_mode = "split",
+		show_prompt = true,
+		show_model = true,
+		no_auto_close = false,
+		debug = true,
+	},
+	keys = {
+		{
+			"<leader>ge",
+			":Gen<CR>",
+			desc = "gen prompt",
+			mode = { "n", "v" },
+		},
+	},
+}
+
+local cmp_ai = {
+	"tzachar/cmp-ai",
+	dependencies = "nvim-lua/plenary.nvim",
+	config = function()
+		local cmp_ai = require("cmp_ai.config")
+
+		cmp_ai:setup({
+			max_lines = 1000,
+			provider = "Codestral",
+			provider_options = {
+				model = gen_nvim_model,
+			},
+			notify = true,
+			notify_callback = function(msg)
+				print(msg)
+				vim.notify(msg)
+			end,
+			run_on_every_keystroke = false,
+		})
+	end,
+}
+
 return {
-  {
-    "nomnivore/ollama.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-
-    -- All the user commands added by the plugin
-    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-    keys = {
-      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-      {
-        "<leader>oo",
-        ":<c-u>lua require('ollama').prompt()<cr>",
-        desc = "ollama prompt",
-        mode = { "n", "v" },
-      },
-
-      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-      {
-        "<leader>oa",
-        ":<c-u>lua require('ollama').prompt('Ask_About_Code')<cr>",
-        desc = "ollama Generate Code",
-        mode = { "n", "v" },
-      },
-    },
-
-    ---@type Ollama.Config
-    opts = {
-      -- your configuration overrides
-      model = "llama3",
-      url = "https://api-ollama-local.thenaman047.dev",
-      -- View the actual default prompts in ./lua/ollama/prompts.lua
-      prompts = ollama_prompts.prompts,
-    },
-  },
-  {
-    "David-Kunz/gen.nvim",
-    opts = {
-      model = "codestral-latest", -- The default model to use.
-      -- host = "api-ollama-local.thenaman047.dev", -- The host running the Ollama service.
-      host = "codestral.mistral.ai", -- The host running the Ollama service.
-      port = "",                  -- The port on which the Ollama service is listening.
-      command = function(options)
-        local body = { model = options.model, stream = true }
-        -- local endpoint = "curl --silent --no-buffer -X POST https://" .. options.host .. "/api/chat -d $body"
-        local mistralAPIKey = os.getenv("MISTRAL_API_KEY")
-        if not mistralAPIKey then
-          print("Error: MISTRAL_API_KEY environment variable is not set.")
-          return nil
-        end
-        local endpoint = "curl --silent --no-buffer --header 'Authorization: Bearer "
-            .. mistralAPIKey
-            .. "' -X POST https://"
-            .. options.host
-            .. "/v1/chat/completions -d $body"
-        return endpoint
-      end,
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a command string.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
-      display_mode = "split", -- The display mode. Can be "float" or "split" or "horizontal-split".
-      show_prompt = true,  -- Shows the prompt submitted to Ollama.
-      show_model = true,   -- Displays which model you are using at the beginning of your chat session.
-      no_auto_close = false, -- Never closes the window automatically.
-      debug = true,        -- Prints errors and the command which is run.
-    },
-    keys = {
-      {
-        "<leader>ge",
-        ":Gen<CR>",
-        desc = "gen prompt",
-        mode = { "n", "v" },
-      },
-    },
-  },
+	ollama_config,
+	gen_nvim_config,
+	-- cmp_ai,
 }
