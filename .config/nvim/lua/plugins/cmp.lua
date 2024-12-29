85c9881929206f06176dfa3775de6555413fa5ff
TheNaman047 <namankhandelwal314@gmail.com>
2024-12-29 18:50:54 +0530

    feat: fixed cmp issues & improvements. added dadbod for db queries in nvim

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,139 +1,145 @@
 local blink_cmp = {
-  "saghen/blink.cmp",
-  version = "*",
-  opts = {},
-  opts_extend = { "sources.default" },
-  lazy = true,
-  dependencies = {
-    "onsails/lspkind.nvim",
-    {
-      "saghen/blink.compat",
-      -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-      version = "*",
-      -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-      lazy = true,
-      -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-      opts = {},
-    },
-    {
-      "L3MON4D3/LuaSnip",
-      dependencies = {
-        "rafamadriz/friendly-snippets",
-      },
-      version = "v2.*",
-      lazy = true,
-    },
-  },
-  config = function()
-    local config = require("blink.cmp")
-    local luasnip = require("luasnip")
-    -- Load user defined snippets
-    require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/nvim/lua/config/snippets" } })
-    config.setup({
-      -- This comes from the luasnip extra, if you don't add it, won't be able to
-      -- jump forward or backward in luasnip snippets
-      -- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
-      }, -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = {
-        preset = "super-tab",
-      },
-      completion = {
-        menu = {
-          -- nvim-cmp style menu
-          draw = {
-            columns = {
-              { "label",     "label_description", gap = 1 },
-              { "kind_icon", "kind",              gap = 1 },
-            },
-            components = {
-              kind_icon = {
-                ellipsis = true,
-                text = function(ctx)
-                  return require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
-                  })
-                end,
-              },
-            },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 500,
-        },
-        list = {
-          selection = "manual",
-        },
-        trigger = {
-          show_in_snippet = false,
-        },
-      },
-      signature = { enabled = false, window = { border = "single" } },
+	"saghen/blink.cmp",
+	version = "*",
+	opts = {},
+	opts_extend = { "sources.default" },
+	lazy = true,
+	dependencies = {
+		"onsails/lspkind.nvim",
+		{
+			"saghen/blink.compat",
+			-- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+			version = "*",
+			-- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+			lazy = true,
+			-- make sure to set opts so that lazy.nvim calls blink.compat's setup
+			opts = {},
+		},
+		{
+			"rafamadriz/friendly-snippets",
+			lazy = true,
+		},
+	},
+	config = function()
+		local config = require("blink.cmp")
+		-- local luasnip = require("luasnip")
+		-- Load user defined snippets
+		-- require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/nvim/lua/config/snippets" } })
+		config.setup({
+			-- This comes from the luasnip extra, if you don't add it, won't be able to
+			-- jump forward or backward in luasnip snippets
+			-- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
+			-- snippets = {
+			-- 	expand = function(snippet)
+			-- 		luasnip.lsp_expand(snippet)
+			-- 	end,
+			-- 	active = function(filter)
+			-- 		if filter and filter.direction then
+			-- 			return luasnip.jumpable(filter.direction)
+			-- 		end
+			-- 		return luasnip.in_snippet()
+			-- 	end,
+			-- 	jump = function(direction)
+			-- 		luasnip.jump(direction)
+			-- 	end,
+			-- }, -- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			-- See the full "keymap" documentation for information on defining your own keymap.
+			keymap = {
+				preset = "super-tab",
+        cmdline = {
+          preset = "enter"
+        }
+			},
+			completion = {
+				menu = {
+					-- nvim-cmp style menu
+					draw = {
+						columns = {
+							{ "label", "label_description", gap = 1 },
+							{ "kind_icon", "kind", gap = 1 },
+						},
+						components = {
+							kind_icon = {
+								ellipsis = true,
+								text = function(ctx)
+									return require("lspkind").symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end,
+							},
+						},
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+				},
+				list = {
+					selection = "manual",
+				},
+				trigger = {
+					show_in_snippet = false,
+				},
+			},
+			signature = { enabled = false, window = { border = "single" } },
 
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer", "luasnip" },
-        providers = {
-          luasnip = {
-            name = "luasnip",
-            enabled = true,
-            module = "blink.cmp.sources.luasnip",
-            min_keyword_length = 2,
-            fallbacks = { "snippets" },
-            score_offset = 95, -- the higher the number, the higher the priority
-          },
-          snippets = {
-            name = "snippets",
-            enabled = true,
-            module = "blink.cmp.sources.snippets",
-            score_offset = 90, -- the higher the number, the higher the priority
-          },
-          path = {
-            name = "Path",
-            module = "blink.cmp.sources.path",
-            score_offset = 3,
-            -- When typing a path, I would get snippets and text in the
-            -- suggestions, I want those to show only if there are no path
-            -- suggestions
-            fallbacks = { "snippets", "luasnip", "buffer" },
-            opts = {
-              trailing_slash = false,
-              label_trailing_slash = true,
-              get_cwd = function(context)
-                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-              end,
-              show_hidden_files_by_default = true,
-            },
-          },
-          buffer = {
-            name = "Buffer",
-            module = "blink.cmp.sources.buffer",
-            min_keyword_length = 2,
-            score_offset = 80,
-          },
-        },
-      },
-    })
-  end,
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "luasnip", "dadbod" },
+				providers = {
+					luasnip = {
+						name = "luasnip",
+						enabled = true,
+						module = "blink.cmp.sources.luasnip",
+						min_keyword_length = 2,
+						fallbacks = { "snippets" },
+						score_offset = 95, -- the higher the number, the higher the priority
+					},
+					snippets = {
+						name = "snippets",
+						enabled = true,
+						module = "blink.cmp.sources.snippets",
+						score_offset = 90, -- the higher the number, the higher the priority
+					},
+					path = {
+						name = "Path",
+						module = "blink.cmp.sources.path",
+						score_offset = 3,
+						-- When typing a path, I would get snippets and text in the
+						-- suggestions, I want those to show only if there are no path
+						-- suggestions
+						fallbacks = { "snippets", "luasnip", "buffer" },
+						opts = {
+							trailing_slash = false,
+							label_trailing_slash = true,
+							get_cwd = function(context)
+								return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+							end,
+							show_hidden_files_by_default = true,
+						},
+					},
+					-- Example on how to configure dadbod found in the main repo
+					-- https://github.com/kristijanhusak/vim-dadbod-completion
+					dadbod = {
+						name = "Dadbod",
+						module = "vim_dadbod_completion.blink",
+						score_offset = 85, -- the higher the number, the higher the priority
+					},
+					buffer = {
+						name = "Buffer",
+						module = "blink.cmp.sources.buffer",
+						min_keyword_length = 2,
+						score_offset = 80,
+					},
+				},
+			},
+		})
+	end,
 }
 
 return {
-  blink_cmp,
+	blink_cmp,
 }
