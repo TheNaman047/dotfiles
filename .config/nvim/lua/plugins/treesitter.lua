013d5bb77f25b2da89b4481a8a9c7f0db114298b
TheNaman047 <namankhandelwal314@gmail.com>
2025-05-11 22:28:26 +0530

    feat: terraform support added

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,35 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"typescript",
-				"bash",
-				"json",
-				"python",
+    configs.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "bash",
+        "json",
+        "python",
         "toml",
-				"rust",
-				"sql",
-				"tsx",
-				"yaml",
-				"css",
-				"html",
-				"dockerfile",
-			},
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+        "rust",
+        "sql",
+        "tsx",
+        "yaml",
+        "css",
+        "html",
+        "dockerfile",
+        "terraform",
+        "hcl"
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
