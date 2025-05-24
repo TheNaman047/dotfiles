a3102405135900294da51b4847a26c7f7544604a
TheNaman047 <namankhandelwal314@gmail.com>
2025-05-24 16:49:49 +0530

    chore: telescope improvements and key map changes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,53 +1,69 @@
 return {
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			-- Load fzf native
-			local telescope = require("telescope")
-			telescope.setup({
-				extensions = {
-					fzf = {
-						fuzzy = true,
-						override_generic_sorter = true,
-						override_file_sorter = true,
-						case_mode = "ignore_case",
-					},
-				},
-			})
-			telescope.load_extension("fzf")
-			-- telescope.load_extension("remote-sshfs")
-
-			-- Setup keymaps
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>f", function()
-				return builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
-			end, {})
-			vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
-			vim.keymap.set(
-				"n",
-				"<leader>j",
-				'<cmd>lua require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}"})<CR>',
-				{ desc = "Live Grep Code" }
-			)
-			vim.keymap.set("n", "<leader>tb", builtin.buffers, { desc = "Find Buffers" })
-			vim.keymap.set("n", "<leader>th", builtin.help_tags, { desc = "Find Help Tags" })
-			vim.keymap.set("n", "<leader>ts", builtin.lsp_document_symbols, { desc = "Find Symbols" })
-			vim.keymap.set("n", "<leader>to", builtin.oldfiles, { desc = "Find Old Files" })
-			vim.keymap.set("n", "<leader>tw", builtin.grep_string, { desc = "Find Word under Cursor" })
-			vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
-			vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
-			vim.keymap.set("n", "<leader>tk", builtin.keymaps, { desc = "Find Keymaps" })
-			vim.keymap.set("n", "<leader>/", function()
-				-- You can pass additional configuration to telescope to change theme, layout, etc.
-				require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-					winblend = 10,
-					previewer = true,
-					layout_config = { width = 0.6 },
-				}))
-			end, { desc = "[/] Fuzzily search in current buffer" })
-		end,
-	},
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>f",  function() require("telescope.builtin").find_files() end,                                 desc = "Find Files" },
+      { "<leader>s",  function() require("telescope.builtin").live_grep() end,                                  desc = "Live Grep" },
+      { "<leader>j",  function() require("telescope.builtin").live_grep({ glob_pattern = "!{spec,test}" }) end, desc = "Live grep code" },
+      { "<leader>tr", function() require("telescope.builtin").resume() end,                                     desc = "Resume search" },
+      { "<leader>tb", function() require("telescope.builtin").buffers() end,                                    desc = "Find Buffers" },
+      { "<leader>th", function() require("telescope.builtin").help_tags() end,                                  desc = "Find Help Tags" },
+      { "<leader>ts", function() require("telescope.builtin").lsp_document_symbols() end,                       desc = "Find Symbols" },
+      { "<leader>to", function() require("telescope.builtin").oldfiles() end,                                   desc = "Find Old Files" },
+      { "<leader>tw", function() require("telescope.builtin").grep_string() end,                                desc = "Find Word under Cursor" },
+      { "<leader>gc", function() require("telescope.builtin").git_commits() end,                                desc = "Search Git Commits" },
+      { "<leader>gb", function() require("telescope.builtin").git_bcommits() end,                               desc = "Search Git Commits for Buffer" },
+      { "<leader>tk", function() require("telescope.builtin").keymaps() end,                                    desc = "Find Keymaps" },
+      { "<leader>tm", function() require("telescope.builtin").colorscheme() end,                                desc = "Change theme" },
+      {
+        "<leader>/",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            winblend = 10,
+            previewer = true,
+            layout_config = { width = 0.6 },
+          }))
+        end,
+        desc = "[/] Fuzzily search in current buffer"
+      },
+    },
+    config = function()
+      -- Load fzf native
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          layout_strategy = 'flex', -- based on width (kinda like this actually and it resizes with the window perfectly)
+          -- layout_strategy = 'vertical', -- default is horizontal (files+prompt left, preview right)
+          -- layout_strategy = 'horizontal', -- vertical = (preview top, files middle, prompt bottom) -- maximizes both list of files and preview content
+          layout_config = {
+            -- :help telescope.layout
+            horizontal = { width = 0.9 },
+            vertical = { width = 0.9 },
+          },
+        },
+        pickers = {
+          live_grep = {
+            layout_strategy = 'vertical',
+          },
+          find_files = {
+            theme = "dropdown",
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "ignore_case",
+          },
+        },
+      })
+      telescope.load_extension("fzf")
+      -- telescope.load_extension("remote-sshfs")
+    end,
+  },
 }
