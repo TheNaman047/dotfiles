46ae670c648a0de35ae4769e6619ec8d7eab9e6f
TheNaman047 <namankhandelwal314@gmail.com>
2025-07-04 18:11:52 +0530

    chore: theme change and music enabled in tmux

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -18,33 +18,53 @@ return {
   --     vim.cmd([[colorscheme rose-pine]])
   --   end,
   -- },
+  {
+    "folke/tokyonight.nvim",
+    opts = {},
+    config = function()
+      local tokyoNight = require("tokyonight")
+      tokyoNight.setup({
+        style = "night",
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
   -- {
-  --   "folke/tokyonight.nvim",
-  --   opts = {},
+  --   "ray-x/aurora",
+  --   init = function()
+  --     vim.g.aurora_italic = 1
+  --     vim.g.aurora_transparent = 1
+  --     -- vim.g.aurora_darker = 1
+  --     -- vim.g.aurora_bold = 1
+  --   end,
   --   config = function()
-  --     local tokyoNight = require("tokyonight")
-  --     tokyoNight.setup({
-  --       style = "night",
-  --       transparent = true,
-  --       styles = {
-  --         sidebars = "transparent",
-  --         floats = "transparent",
-  --       },
-  --     })
-  --     vim.cmd([[colorscheme tokyonight]])
+  --     vim.cmd([[colorscheme aurora]])
+  --     -- Override problematic highlight groups
+  --     local overrides = {
+  --       -- Common problematic groups
+  --       ["@number"] = { fg = "#e933e3" },
+  --       -- ["@string"] = { fg = "#98c379" },
+  --       -- ["@comment"] = { fg = "#7c7c7c", italic = true },
+  --       -- ["Normal"] = { fg = "#abb2bf" },
+  --       -- ["Identifier"] = { fg = "#e06c75" },
+  --       -- ["Function"] = { fg = "#61afef" },
+  --       -- ["Keyword"] = { fg = "#c678dd" },
+  --       -- ["Type"] = { fg = "#e5c07b" },
+  --       -- ["Constant"] = { fg = "#d19a66" },
+  --       -- ["Special"] = { fg = "#56b6c2" },
+  --       -- -- Add background for better contrast if needed
+  --       -- ["CursorLine"] = { bg = "#2c323c" },
+  --       -- ["Visual"] = { bg = "#3e4451" },
+  --     }
+  --
+  --     for group, opts in pairs(overrides) do
+  --       vim.api.nvim_set_hl(0, group, opts)
+  --     end
   --   end,
   -- },
-  {
-  	"ray-x/aurora",
-  	init = function()
-  		vim.g.aurora_italic = 1
-  		vim.g.aurora_transparent = 1
-  		-- vim.g.aurora_bold = 1
-  	end,
-  	config = function()
-      vim.cmd([[colorscheme aurora]])
-  		-- override defaults
-  		vim.api.nvim_set_hl(0, "@number", { fg = "#e933e3" })
-  	end,
-  },
 }
