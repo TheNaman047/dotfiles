f9569a2f77806bde5b4716eb3b9e904d32e3b0bc
TheNaman047 <namankhandelwal314@gmail.com>
2026-08-21 02:23:50 +0530

    Revert "Merge pull request #30 from TheNaman047/worktree-typst-preview"
    This reverts commit 877560403a2dced82b267b305891fe0416f868ca, reversing
    changes made to e33e3e06692d46edc4b7bec9d8aad1dd050af9a7.

--- .config/nvim/lua/plugins/typst.lua
+++
@@ -1,8 +0,0 @@
-vim.pack.add({
-  { src = "https://github.com/chomosuke/typst-preview.nvim", version = vim.version.range("1.*") },
-})
-
--- setup() is what fetches/refreshes the tinymist + websocat binaries.
-require("typst-preview").setup({})
-
-vim.keymap.set("n", "<leader>mt", "<cmd>TypstPreviewToggle<CR>", { desc = "Toggle typst preview" })
